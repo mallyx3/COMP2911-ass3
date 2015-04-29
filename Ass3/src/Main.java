5fea40f8beb2bbed23922126dd2e0b21cf9ece83
Tomas Donovic <tomasdonovic@gmail.com>
2015-04-29 18:36:52 +1000

    Revert "Added code to take board size input and create array.  Task 1 complete. 30 minutes elapsed."
    This reverts commit 7f40e64c9a9688fa6d60df1c2b618b9eb97ea64c.

--- Ass3/src/Main.java
+++ Ass3/src/Main.java
@@ -1,8 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-
 /**
  * 
  */
@@ -15,73 +10,10 @@ public class Main {
 
 	/**
 	 * @param args
-	 *
 	 */
-	
-	/*
-	 
-	------------------------------------------------
-	No.:			1 <---
-	Task:			Generate blank board
-	Difficulty:		1
-	Time:			1
-	Dependent on:	9
-	Comment:		Create a 2d array, null, p1, p2
-	Assigned to:	Tom
-	Note: 			In Main
-	------------------------------------------------
-	
-	*/
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//Ask for the x and y sizes
-		//Modified from code found at http://www.java2s.com/Code/Java/File-Input-Output/ReadastringfromconsoleusingaBufferedReader.htm
-		String prompt = "How wide do you want the board?";
-		System.out.print(prompt);
-		 
-	      //  open up standard input
-	      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	 
-	      String xSize = null;
-	 
-	      //  read the username from the command-line; need to use try/catch with the
-	      //  readLine() method
-	      try {
-	         xSize = br.readLine();
-	      } catch (IOException ioe) {
-	         System.out.println("IO error trying to get size!");
-	         System.exit(1);
-	      }
-	      
-		Integer x = Integer.parseInt(xSize);
-		
-		prompt = "How tall do you want the board?";
-		System.out.print(prompt);
-		 
-	      //  open up standard input
-	      br = new BufferedReader(new InputStreamReader(System.in));
-	 
-	      xSize = null;
-	 
-	      //  read the username from the command-line; need to use try/catch with the
-	      //  readLine() method
-	      try {
-	         xSize = br.readLine();
-	      } catch (IOException ioe) {
-	         System.out.println("IO error trying to get size!");
-	         System.exit(1);
-	      }
-		
-		Integer y = Integer.parseInt(xSize);
-	      
-	     //Now the board array is created.
-		Integer[][] board = new Integer[x][y];
-		
-		
-	}
 
-	
+	}
 
 }

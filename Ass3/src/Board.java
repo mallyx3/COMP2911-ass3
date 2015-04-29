7f40e64c9a9688fa6d60df1c2b618b9eb97ea64c
Tomas Donovic <tomasdonovic@gmail.com>
2015-04-29 18:36:28 +1000

    Added code to take board size input and create array.  Task 1 complete. 30 minutes elapsed.

---
+++ Ass3/src/Board.java
@@ -0,0 +1,32 @@
+
+public class Board {
+	Integer x;
+	Integer y;
+	Integer[][] board = new Integer[x][y];
+	/**
+	 * @return the x
+	 */
+	public Integer getX() {
+		return x;
+	}
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(Integer x) {
+		this.x = x;
+	}
+	/**
+	 * @return the y
+	 */
+	public Integer getY() {
+		return y;
+	}
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(Integer y) {
+		this.y = y;
+	}
+	
+	
+}
